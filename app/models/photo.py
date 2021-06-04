c14e280c35cf828246db1474815936dc99a0830a
Drewthurm21 <drewthurm21@gmail.com>
2021-06-04 10:10:22 -0700

    added comment model to database

--- app/models/photo.py
+++ app/models/photo.py
@@ -5,10 +5,12 @@ class Photo(db.Model):
     __tablename__ = 'photos'
 
     id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
     photo_url = db.Column(db.Text, nullable = False, unique = True)
 
     def to_dict(self):
         return {
             "id": self.id,
+            "name": self.name,
             "photo_url": self.photo_url
         }
