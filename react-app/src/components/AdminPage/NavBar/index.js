50a78ad2ffe114d394553402eea3111ca6b8243d
Drewthurm21 <drewthurm21@gmail.com>
2021-06-08 18:27:16 -0700

    created PostForm modal and hooked it up to admin page

--- react-app/src/components/AdminPage/NavBar/index.js
+++ react-app/src/components/AdminPage/NavBar/index.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 import { logout } from "../../../store/session";
+import { showModal, setCurrentModal } from '../../../store/modals'
+import PostModal from '../PostModal'
 import './NavBar.css'
+
+
+
 const NavBar = () => {
   const dispatch = useDispatch();
-  
+
   const onLogout = async (e) => {
     await dispatch(logout());
   };
 
+  const makePost = () => {
+    dispatch(setCurrentModal(PostModal))
+    dispatch(showModal())
+  }
+
   return (
     <nav className='nav-links-container'>
-        <NavLink className='nav-link' to="/" exact={true} activeClassName="active">
-            Home
+      <NavLink className='nav-link' to="/" exact={true} activeClassName="active">
+        Home
           </NavLink>
-        <NavLink className='nav-link' to="/" exact={true} activeClassName="active">
-          Create Post
-          </NavLink>
-       
-        <NavLink className='nav-link' to="/" onClick={onLogout} exact={true} activeClassName="active">
-          Log Out
+      <div className='nav-link' onClick={makePost} exact={true} activeClassName="active">
+        Create Post
+          </div>
+
+      <NavLink className='nav-link' to="/" onClick={onLogout} exact={true} activeClassName="active">
+        Log Out
           </NavLink>
     </nav>
   );
