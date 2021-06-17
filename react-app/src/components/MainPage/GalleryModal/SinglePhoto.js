07b59392859ee5e2e13211e0e903a46f653acfe8
Drewthurm21 <drewthurm21@gmail.com>
2021-06-17 08:06:11 -0700

    hooked up single photo modal and adjusted size

--- react-app/src/components/MainPage/GalleryModal/SinglePhoto.js
+++ react-app/src/components/MainPage/GalleryModal/SinglePhoto.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux'
 
+const SinglePhotoModal = () => {
 
-const SinglePhotoModal = ({ photo }) => {
-
-
+  const photo = useSelector(state => state.photos.photo)
 
   return (
-    <div> yo </div>
+    <div className='single-img-container'>
+      <img className='single-img' src={photo.photo_url} alt=''></img>
+    </div>
   )
 }
 
--- react-app/src/components/MainPage/GalleryModal/index.js
+++ react-app/src/components/MainPage/GalleryModal/index.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SinglePhotoModal from './SinglePhoto'
-import { hideModal, setCurrentModal, showModal } from "../../../store/modals";
-import { getPhotos } from '../../../store/photos'
+import { setCurrentModal } from "../../../store/modals";
+import { getPhotos, getPhoto } from '../../../store/photos'
 import './GalleryModal.css';
 
 const GalleryModal = () => {
     const collageClasses = ['row1-L', 'row1-S', 'row2-L', 'row2-R', 'row3-S', 'row3-L'];
     const dispatch = useDispatch();
     const photos = useSelector(state => state.photos.photos);
-    if (photos) console.log(photos[0].photo_url)
 
-    let showPhoto = () => {
+    let showPhoto = async (photo) => {
+        console.log(photo.id)
+        await dispatch(getPhoto(photo.id))
         dispatch(setCurrentModal(SinglePhotoModal))
-
     }
 
     useEffect(() => {
