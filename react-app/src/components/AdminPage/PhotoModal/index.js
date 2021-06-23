22ca31f48250caaaec8532ec39984127bf27381f
eramsay20 <eramsay10@gmail.com>
2021-06-23 14:12:46 -0400

    updated upload photo module css

--- react-app/src/components/AdminPage/PhotoModal/index.js
+++ react-app/src/components/AdminPage/PhotoModal/index.js
@@ -32,19 +32,21 @@ const PhotoModal = () => {
     <div className='blog-form-container'>
       <form className='blog-form-form'>
         <h2 className='blog-form-header-underline'>Upload New Image</h2>
+  
         <div className='blog-form-form-inputs-container'>
-          <div style={{ 'marginLeft': '20px' }} className='blog-form-form-input'>
+          <div className='blog-form-form-input'>
             <label>TITLE</label>
             <input value={title} className='upload-image-btn' type='text' onChange={e => setTitle(e.target.value)} ></input>
           </div>
-          <div style={{ 'marginLeft': '20px' }} className='blog-form-form-input'>
-            <label>PHOTO</label>
+          <div className='blog-form-form-input'>
+            <label>PHOTO SELECT</label>
             <input className='upload-image-btn' type='file' accept='image/*' id='imageUpload' onChange={updateImage} ></input>
           </div>
         </div>
 
-        <div className='preview-img-container'>PREVIEW
-          <img className='preview-img' id='imgPreview' src='#' alt='img'></img>
+        <div className='preview-img-container'>
+          <h4>PREVIEW</h4>
+          <img className='preview-img' id='imgPreview' src='#' alt='image upload preview'></img>
         </div>
 
         {image &&
@@ -52,9 +54,9 @@ const PhotoModal = () => {
             <div className='blog-form-form-btn' style={{ 'marginRight': '20px' }} onClick={postPhoto} required >UPLOAD PHOTO</div>
           </div>}
 
-        <div className='blog-form-social-container'>
-          <div className='blog-form-form-btn' onClick={closeForm}>CANCEL</div>
-        </div>
+        
+        <div className='blog-form-form-btn' onClick={closeForm}>CANCEL</div>
+        
       </form>
     </div >
   );
